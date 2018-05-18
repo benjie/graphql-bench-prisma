e62dabf1b6174c2239f940c5c0d9e45078960353
Benjie Gillam <benjie@jemjie.com>
2018-05-18 12:45:36 +0100

    PostGraphile setup scripts

---
+++ testcandidates/postgraphile/setup/manage.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+usage() {
+    echo "Usage: $0 (init|start|nuke)"
+}
+
+init() {
+    docker build -t "postgraphile:latest" .
+    docker-compose down -v --remove-orphans
+    docker-compose up -d
+    # Give PostgreSQL enough time to start
+    sleep 30
+    psql -1X -v ON_ERROR_STOP=1 'postgres://postgres:unsecured@localhost:5432/chinook' < ../import/chinook_import
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+case $1 in
+    init)
+        init
+        exit
+        ;;
+    start)
+        docker-compose down -v --remove-orphans
+        docker-compose up -d
+        exit
+        ;;
+    nuke)
+        docker-compose down -v --remove-orphans
+        exit
+        ;;
+    *)
+        echo "unexpected option: $1"
+        usage
+        exit 1
+        ;;
+esac
