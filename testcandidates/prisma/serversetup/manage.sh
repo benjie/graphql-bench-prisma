114ed741a1f65d80850036283454435e3420e79c
Benjie Gillam <benjie@jemjie.com>
2018-05-18 13:09:24 +0100

    Prisma's (broken?) config

---
+++ testcandidates/prisma/serversetup/manage.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+
+cd "$(dirname "$0")"
+
+set -e
+
+usage() {
+    echo "Usage: $0 (init|start|nuke)"
+}
+
+init() {
+    mkdir -p /tmp/prisma
+    npm install prisma --prefix=/tmp/prisma
+    PRISMA=/tmp/prisma/node_modules/prisma/dist/index.js
+    "$PRISMA" local nuke
+    "$PRISMA" deploy
+    "$PRISMA" import --data ../import/chinook.zip
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+case $1 in
+    init)
+        init
+        exit
+        ;;
+    start)
+        docker start prisma-db
+        docker start local_prisma-database_1
+        exit
+        ;;
+    stop)
+        docker stop local_prisma-database_1
+        docker stop prisma-db
+        exit
+        ;;
+    nuke)
+        docker stop prisma-db && docker rm prisma-db
+        docker stop local_prisma-database_1 && docker rm local_prisma-database_1
+        exit
+        ;;
+    *)
+        echo "unexpected option: $1"
+        usage
+        exit 1
+        ;;
+esac
