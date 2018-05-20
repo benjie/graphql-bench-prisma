6a2f9a18540276c094e61e7aff0d515f859e1305
Benjie Gillam <benjie@jemjie.com>
2018-05-20 15:09:57 +0100

    Display tweaks

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -6,17 +6,34 @@ import 'chartist/dist/chartist.min.css';
 import allData from './data.json';
 import uniq from 'lodash/uniq';
 import groupBy from 'lodash/groupBy';
+import Chartist from 'chartist';
 
 const QUERIES = Object.keys(allData);
-const ALL_STATS = [
-  "latMean",
-  "lat50",
-  "lat95",
-  "lat99",
-  "latMax",
-  "success",
-  "failure",
-];
+
+const makeOptions = o => {
+  return {
+    ...o,
+    options: {
+      ...o.options,
+      axisX: {
+        type: Chartist.FixedScaleAxis,
+        onlyInteger: true,
+        ...o.axisX
+      },
+    }
+  };
+}
+const CONFIG_BY_STAT = {
+  "latMean": makeOptions({}),
+  "lat50": makeOptions({}),
+  "lat95": makeOptions({}),
+  "lat99": makeOptions({}),
+  "latMax": makeOptions({}),
+  "success": makeOptions({}),
+  "failure": makeOptions({}),
+};
+
+const ALL_STATS = Object.keys(CONFIG_BY_STAT);
 
 class App extends Component {
   state = {
@@ -39,20 +56,35 @@ class App extends Component {
     const groupedEntries = groupBy(entries, e => e.software);
     const softwares = Object.keys(groupedEntries);
     const data = {
-      labels: rps,
+      labels: rps.map((r, i) => i % 2 === 1 ? r : ''),
       series: softwares.map(
-        software => rps.map(
-          r => {
-            const entry = groupedEntries[software].find(e => e.rps === r)
-            if (entry) {
-              return entry[this.state.stat];
-            } else {
-              return null;
+        software => ({
+          name: software,
+          className: `series-${software.replace(/[^a-z0-9]/g, '-')}`,
+          data: rps.map(
+            r => {
+              const entry = groupedEntries[software].find(e => e.rps === r)
+              if (entry) {
+                return {x: r, y: entry[this.state.stat]};
+              } else {
+                return {x: r, y: null};
+              }
             }
-          }
-        )
+          ).filter(p => p.y !== null)
+        })
       )
     }
+    const {options: baseOptions, title} = CONFIG_BY_STAT[this.state.stat] || {};
+    const options = {
+      ...baseOptions,
+      axisX: {
+        ...baseOptions.axisX,
+        ticks:  rps.filter((r, i) => i % 2 === 1),
+        low: 0,
+        high: rps[rps.length - 1],
+      }
+    };
+        console.log(  rps.filter((r, i) => i % 2 === 1))
     return (
       <div className="App">
         <div style={{width: '100%', height: '10vh'}}>
@@ -63,10 +95,24 @@ class App extends Component {
             {ALL_STATS.map(q => <option key={q} value={q}>{q}</option>)}
           </select>
         </div>
-        <div style={{width: '100%', height: '90vh'}}>
-          <ChartistGraph data={
-            data
-          } type={'Line'} />
+        <div style={{display: 'flex', height: '90vh', alignItems: 'stretch'}}>
+          <div style={{flex: '1 0 0'}}>
+            <ChartistGraph options={options} data={
+              data
+            } type={'Line'} />
+          </div>
+          <div style={{flex: '0 0 100px'}}>
+            <h3>Legend</h3>
+            <div className='label-postgraphile'>
+              postgraphile
+            </div>
+            <div className='label-postgraphile-next'>
+              postgraphile-next
+            </div>
+            <div className='label-prisma'>
+              prisma
+            </div>
+          </div>
         </div>
       </div>
     );
