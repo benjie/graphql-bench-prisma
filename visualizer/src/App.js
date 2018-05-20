52e6a76d86bcf011bc53d631c9db269c62ce795b
Benjie Gillam <benjie@jemjie.com>
2018-05-20 11:36:38 +0100

    Simple graph

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChartistGraph from 'react-chartist';
+import 'chartist/dist/chartist.min.css';
 
 class App extends Component {
   render() {
+    const data = {
+      labels: 'MTWTFSS'.split(''),
+      series: [
+        [12, 9 ,7 ,8 ,5],
+        [2, 1, 3.5, 7, 3],
+        [1, 3, 4, 5, 6]
+      ]
+    };
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChartistGraph data={
+          data
+        } type={'Line'} />
       </div>
     );
   }
